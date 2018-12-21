dc1558f87d8939324534cae537f33e156fb23225
Unknown <delasalle.sio.colleu.e@gmail.com>
2018-12-21 11:34:17 +0100

    Cube animation

--- index.php
+++ index.php
@@ -2,5 +2,9 @@
     //inclusion du header
     include('include/head.php');
 ?>
-<!DOCTYPE HTML> 
+<!DOCTYPE HTML>
 <html>
+	<body>
+		<img src="Images/Portal_CompanionCube.png" class = 'cube'>
+	</body> 
+</html>
