5f21e2be3efbb9568d0b9ea99b3f8679c6421e3f
Unknown <delasalle.sio.colleu.e@gmail.com>
2018-12-21 10:05:20 +0100

    Head création

---
+++ include/head.php
@@ -0,0 +1,14 @@
+<head>
+	<!-- Title -->
+	<title>Portal Animation</title>
+		
+	<!-- Type -->
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	
+	<!-- Settings -->
+	<meta name="Content-Language" content="fr">  
+	<meta name="Author" content="Colleu Elven, Couillard Théo">
+	
+	<!-- access to style sheets -->
+	<link href="style/style.css" rel="stylesheet" type="text/css" />	
+</head>
