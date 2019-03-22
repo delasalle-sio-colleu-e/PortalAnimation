bad2f6e8064748abc3c9dd682363ba5d13bdba04
Unknown <delasalle.sio.colleu.e@gmail.com>
2019-03-22 10:06:03 +0100

    Auto stash before merge of "master" and "origin/master"

--- index.php
+++ index.php
@@ -7,5 +7,22 @@
 	<body>
 		<img src="Images/Portal_CompanionCube.png" class = 'cube'>
 		<img src="Images/bonhomme1.png" class = 'bonhomme'>
+		
+		<script>
+			function myMove() {
+			  var elem = document.getElementById("animate");   
+			  var pos = 0;
+			  var id = setInterval(frame, 5);
+			  function frame() {
+				if (pos == 350) {
+				  clearInterval(id);
+				} else {
+				  pos++; 
+				  elem.style.top = pos + "px"; 
+				  elem.style.left = pos + "px"; 
+				}
+			  }
+			}
+		</script>
 	</body> 
 </html>
